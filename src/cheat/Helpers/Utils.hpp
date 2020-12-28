d5f590aa4c54e3679849702850857e5fd1825545
CredixYt <credixyt@pm.me>
2020-12-28 16:48:24 +0100

    Added CallVirtualFunction to Utils

---
+++ src/cheat/Helpers/Utils.hpp
@@ -0,0 +1,19 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include "../Platform/CallingConventions.hpp"
+
+#include <cassert>
+
+template <unsigned int nIndex, typename ReturnType, typename... Args>
+FORCEINLINE ReturnType CallVirtualFunction(void* pClass, Args... args) {
+	assert(pClass != nullptr);
+	assert(nIndex >= 0);
+
+	typedef ReturnType(THISCALL* Fn)(void*, Args...);
+
+	auto function = (*(Fn**)pClass)[nIndex];
+	return function(pClass, args...);
+}
+
+#endif
